d6b12fdb649e1829b261d65a589d20ff824c7c79
Sachin Gwasikoti <sachingw777@gmail.com>
2019-02-26 17:34:12 +0530

    word wise

---
+++ Strings/reverse_string_wordwise.cpp
@@ -0,0 +1,32 @@
+#include <string.h>
+void reverse(char input[], int i, int j) {
+	while(i < j) {
+		int temp = input[j];
+		input[j] = input[i];
+		input[i] = temp;
+		i++;
+		j--;
+	}
+}
+
+void reverseStringWordWise(char input[]) {
+    // Write your code here
+  int i =0, j = strlen(input) - 1;
+  int length = strlen(input);
+  reverse(input, i, j);
+
+  i =0,j=0;
+  int index = 0;
+  while(index < length ) {
+	if(input[index] == ' ') {
+      j = index - 1;
+      reverse(input, i, j);
+      i = index + 1;
+      j = index + 1;
+    }
+  	index++;
+  }
+
+  j = index - 1;
+  reverse(input, i, j);
+}
