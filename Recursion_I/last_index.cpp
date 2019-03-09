67933a8650f59f5e7eb80d97e1e9ebe3a83e745c
Sachin Gwasikoti <sachingw777@gmail.com>
2019-03-09 20:38:29 +0530

    done

---
+++ Recursion_I/last_index.cpp
@@ -0,0 +1,16 @@
+int lastIndex(int input[], int size, int x) {
+  if(size == 0){
+    return -1;
+  }
+ 
+  int ans = lastIndex(input+1, size-1, x);
+  
+  if(ans != -1){
+	return ++ans;
+  }
+  if(input[0] == x){
+    return 0;
+  }else{
+    return -1;
+  }
+}
