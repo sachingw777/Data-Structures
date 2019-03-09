5d686e6cd306f70c9e7d1d17532809423adaf728
Sachin Gwasikoti <sachingw777@gmail.com>
2019-03-09 19:09:01 +0530

    done

---
+++ Recursion_I/check_number.cpp
@@ -0,0 +1,23 @@
+/*
+bool checkNumber(int input[], int size, int x) {
+  
+  if(size == 0){
+      return false;
+    }
+  if(input[0] == x){
+    return true;
+  }
+  checkNumber(input + 1, size - 1, x);
+}  
+  */
+
+bool checkNumber(int input[], int size, int x) {
+      if(size == 0){
+      return false;
+    }
+      if(input[0] == x){
+    return true;
+  }
+    bool small = checkNumber(input+1,size-1,x);
+    return small;
+}
