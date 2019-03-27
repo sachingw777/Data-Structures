16126873c6da835575c97e79ba672516e219ca2f
Sachin Gwasikoti <sachingw777@gmail.com>
2019-03-27 12:49:33 +0530

    tender surrender

---
+++ OOPS_II/DynamicArray.cpp
@@ -0,0 +1,78 @@
+class DynamicArray{
+    int *data;
+    int nextIndex;
+    int capacity;
+
+    public:
+
+    DynamicArray(){
+        capacity = 5;
+        data = new int[capacity];
+        nextIndex = 0;
+    }
+
+    DynamicArray(DynamicArray const & d){
+        // this -> data = d.data;      //shallow copy
+        this-> data = new int[d.capacity];
+        for(int i = 0; i < nextIndex; i++){
+            this->data[i] = d.data[i];
+        }
+        this -> nextIndex = d.nextIndex;
+        this -> capacity = d.capacity;
+    }
+
+    void operator=(DynamicArray const & d){
+        this-> data = new int[d.capacity];
+        for(int i = 0; i < nextIndex; i++){
+            this->data[i] = d.data[i];
+        }
+        this -> nextIndex = d.nextIndex;
+        this -> capacity = d.capacity;        
+    }
+
+    void add(int num){
+        if(nextIndex == capacity){  
+        //double the array
+            int *newdata = new int[capacity * 2];
+            for(int i = 0; i < nextIndex; i++){
+                newdata[i] = data[i];
+            }
+        delete [] data;
+        data = newdata;
+        }
+
+        data[nextIndex] = num;
+        nextIndex++;
+    }
+
+    int get(int i){
+        if(i < nextIndex){
+            return data[i];
+        }
+        else if(i <= nextIndex){
+            return -1;
+        }
+    }
+
+    void add(int i, int element){
+        if(i < nextIndex){
+            data[i] = element;
+        }
+        else if(i > nextIndex){
+            return;
+        }
+        else
+        {
+            add(element);
+        }
+    }
+
+    void print(){
+        for(int i = 0; i < nextIndex; i++){
+            cout << data[i] << " ";
+        }
+        cout << endl;
+    }
+
+
+};
---
+++ OOPS_II/DynamicArrayUse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include "DynamicArray.cpp"
+
+int main(){
+    DynamicArray d1;
+    d1.add(10);
+    d1.add(10);
+    d1.add(10);
+    d1.add(10);
+    d1.add(10);
+    d1.add(10);
+d1.print();
+}
+    d1.get(3);
+
