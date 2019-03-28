c5e7b326c971a6a45df2745c6afed135948f1693
Sachin Gwasikoti <sachingw777@gmail.com>
2019-03-28 06:27:37 +0530

    complexity imprvesd-takeInput_Better

--- Linked_List_I/LinkedList.cpp
+++ Linked_List_I/LinkedList.cpp
@@ -2,6 +2,48 @@
 using namespace std;
 #include "Node.cpp"
 
+Node *takeInput_Better(){
+    int data;
+    cin >> data;
+
+    Node *head = NULL; 
+    Node *tail = NULL; 
+
+    while(data != -1){
+        Node * newNode = new Node(data);
+        if(head == NULL){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+Node *takeInput(){
+    int data;
+    cin >> data;
+
+    Node *head = NULL; 
+    while(data != -1){
+        Node * newNode = new Node(data);
+        if(head == NULL){
+            head = newNode;
+        }else{
+            Node *temp = head;
+            while(temp->next != NULL){
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
 void print(Node *head){
     Node * temp = head;
     while(temp != NULL){
