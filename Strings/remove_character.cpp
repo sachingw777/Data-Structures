80c17fe0bf779e25d21a8d69d0735bbeea42969c
Sachin Gwasikoti <sachingw777@gmail.com>
2019-07-21 23:18:33 +0530

    done

---
+++ Strings/remove_character.cpp
@@ -0,0 +1,22 @@
+int length1(char input[]){
+  int len=0;
+  for(int i = 0; input[i] != '\0'; i++){
+    len++;
+    }
+  return len;
+}
+
+
+void removeAllOccurrencesOfChar(char input[], char c) {
+  int i = 0, j = 0;
+  int length = length1(input);
+  while(i < length){
+    if(input[i] != c){
+		input[j] = input[i];
+      j++;i++;
+    }else{
+      i++;
+    }
+  }
+  input[j] = '\0';
+}
