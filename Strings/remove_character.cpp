34dc02bf0051a40f0ea286bee1f5963a0fd8af24
Sachin Gwasikoti <sachingw777@gmail.com>
2019-07-24 17:13:09 +0530

    once over

--- Strings/remove_character.cpp
+++ Strings/remove_character.cpp
@@ -20,3 +20,15 @@ void removeAllOccurrencesOfChar(char input[], char c) {
   }
   input[j] = '\0';
 }
+
+/*
+void removeAllOccurrencesOfChar(char input[], char c) {
+    int j = 0;
+    for(int i = 0; input[i] != '\0'; i++){
+        if(input[i] != c){
+            input[j] = input[i];
+            j++;
+        }
+    }
+    input[j] = '\0';
+} */
