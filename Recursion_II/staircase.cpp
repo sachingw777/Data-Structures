ea2c290c31b0a5014c972f2e912c1d3942df7cb9
Sachin Gwasikoti <sachingw777@gmail.com>
2019-08-31 19:21:22 +0530

    2 done

---
+++ Recursion_II/staircase.cpp
@@ -0,0 +1,11 @@
+int staircase(int n){
+    if(n < 0){
+        return 0;
+    }
+    
+    if(n == 0){
+        return 1;
+    }
+    
+    return staircase(n-1) + staircase(n-2) + staircase(n-3);    
+}
