7d92e2d35635e72098b0a179eece50c7bb824258
Sachin Gwasikoti <sachingw777@gmail.com>
2019-10-01 05:58:40 +0530

    Add MergeSortLL

--- Linked_List_II/LinkedList_II.cpp
+++ Linked_List_II/LinkedList_II.cpp
@@ -61,30 +61,20 @@ Node* mergeTwoLLs(Node *head1, Node *head2) {
     return finalHead;
 }
 
-Node* mergeTwoLLs(Node *head1, Node *head2) {
-    Node *finalHead = NULL, *finalTail = NULL;
-    if(head1 -> data <= head2 -> data){
-        finalHead = head1;
-        finalTail = head1;
-        head1 = head1 -> next;
-    }else{
-        finalHead = head2;
-        finalTail = head2;
-        head2 = head2 -> next;
+Node* mergeSort(Node *head) {
+    if(head == NULL || head -> next == NULL){
+        return head;
     }
 
-    while(head1 != NULL && head2 != NULL){
-        if(head1 -> data <= head2 -> data){
-            finalTail -> next = head1;
-            head1 = head1 -> next;
-            finalTail = finalTail -> next;
-        }else if(head1 -> data > head2 -> data)
-        {
-            finalTail -> next = head2;
-            head2 = head2 -> next;
-            finalTail = finalTail -> next;
-        }
-    }
+    Node *midpoint = midpoint_linkedlist(head);   //midpoints h1, h2
+    Node *h2 = midpoint -> next;
+    midpoint -> next = NULL;
+    
+    Node *first = mergeSort(head);        //RC
+    Node *second = mergeSort(h2);
+    
+    return mergeTwoLLs(first, second);
+}
 
     if(head1 != NULL){
         finalTail -> next= head1;
