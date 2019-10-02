5355fc2b887bb64dd543878e902720cfc44a9f3f
Sachin Gwasikoti <sachingw777@gmail.com>
2019-10-03 04:54:24 +0530

    done. v1.

--- Stacks_Queues/StackUsingArray.cpp
+++ Stacks_Queues/StackUsingArray.cpp
@@ -15,4 +15,34 @@ class StackUsingArray{
         int size(){
             return nextIndex;
         }
+
+        bool isEmpty(){
+            return nextIndex == 0;
+        }
+
+        int top(){
+            if(isEmpty()){
+                cout << "Stack Empty"
+                return INT_MIN;
+            }
+
+            return data[nextIndex - 1];
+        }
+
+        void push(int element){
+            if(nextIndex == capacity){
+                cout << "Stack is Full";
+            }
+            data[nextIndex] = element;
+            nextIndex++;
+        }
+
+        int pop(){
+            if(isEmpty()){
+                cout << "Stack Empty"
+                return INT_MIN;
+            }
+            nextIndex--;
+            return data[nextIndex];
+        }
 }
