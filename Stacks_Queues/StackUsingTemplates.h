4ad122d867087e2c28971a51fff6138ad44803ec
Sachin Gwasikoti <sachingw777@gmail.com>
2019-10-03 12:08:21 +0530

    done.shotgun.

---
+++ Stacks_Queues/StackUsingTemplates.h
@@ -0,0 +1,50 @@
+template <typename T>
+class StackUsingTemplates{
+    T *data;
+    int nextIndex;
+    int capacity;
+
+    public:
+        StackUsingTemplates(){
+            data = new T[5];
+            nextIndex = 0;
+            capacity = 5;
+        }
+
+        int size(){
+            return nextIndex;
+        }
+
+        bool isEmpty(){
+            return nextIndex == 0;
+        }
+
+        T top(){
+            if(isEmpty()){
+                cout << "Stack Empty";
+                return 0;   // 0 works great for all data types. Because it is using ASCII Value.
+            }
+
+            return data[nextIndex - 1];
+        }
+
+        void push(T element){
+            if(nextIndex == capacity){
+                // cout << "Stack is Full";
+                T *newData = new T[capacity * 2];
+                delete [] data;
+                data = newData;
+            }
+            data[nextIndex] = element;
+            nextIndex++;
+        }
+
+        T pop(){
+            if(isEmpty()){
+                cout << "Stack Empty";
+                return 0;
+            }
+            nextIndex--;
+            return data[nextIndex];
+        }
+};
