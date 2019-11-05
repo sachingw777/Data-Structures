7169b2f0d8fc47a4ebac3a12e7dbad39f36c4908
Sachin Gwasikoti <sachingw777@gmail.com>
2019-11-05 20:51:00 +0530

    done.

--- Trees/TreeUseExtra.cpp
+++ Trees/TreeUseExtra.cpp
@@ -11,3 +11,16 @@ bool containsX(TreeNode<int>* root, int x) {
     }
     return false;
 }
+
+int nodesGreaterThanX(TreeNode<int> *root, int x) {
+    int count;
+    if(root -> data > x){
+        count = 1;
+    }else{
+        count = 0;
+    } 
+    for(int i = 0; i < root -> numChildren(); i++){
+        count += nodesGreaterThanX(root -> getChild(i), x);
+    }
+    return count;
+}
