db7177cf88940d6f398278e3fcc8605750807fb6
Sachin Gwasikoti <sachingw777@gmail.com>
2019-11-05 20:00:59 +0530

    small fix.

--- Trees/TreeUse.cpp
+++ Trees/TreeUse.cpp
@@ -85,6 +85,9 @@ void printTree(TreeNode<int> * root){
     }
 }
 int countNodes(TreeNode<int>* root){
+    if(root == NULL){
+        return 0;
+    }
     int ans = 1;
     for(int i = 0; i < root -> children.size(); i++){
         ans += countNodes(root -> children[i]);
