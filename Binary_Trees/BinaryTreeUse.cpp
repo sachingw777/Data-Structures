c4249200f905a197e9090cef6dc37f560deca3c3
Sachin Gwasikoti <sachingw777@gmail.com>
2019-11-09 20:10:41 +0530

    done

--- Binary_Trees/BinaryTreeUse.cpp
+++ Binary_Trees/BinaryTreeUse.cpp
@@ -1,6 +1,76 @@
 #include <iostream>
 #include "BinaryTreeNode.h"
+#include <queue>
 using namespace std;
+
+void printTreeLevelWise(BinaryTreeNode<int>* root){
+	if(root == NULL){
+		return;
+	}
+	queue<BinaryTreeNode<int>*> pendingNodes;
+	pendingNodes.push(root);
+	
+	while(pendingNodes.size() != 0 ){
+		BinaryTreeNode<int>* front = pendingNodes.front();
+		pendingNodes.pop();
+
+		cout << front -> data << ":";
+		cout << "L:"; 
+		if(front -> left){
+			cout << front -> left -> data << ",";	
+			pendingNodes.push(front -> left);
+		}else{
+			cout << -1 << ",";	
+		}
+
+		cout << "R:"; 
+		if(front -> right){
+			cout << front -> right -> data << endl;	
+			pendingNodes.push(front -> right);
+		}else{
+			cout << -1 << endl;	
+		}
+	}	
+}
+
+BinaryTreeNode<int>* takeInputLevelWise(){
+	int rootData;
+	cout << "Enter data" << endl;
+	cin >> rootData;
+
+	if (rootData == -1) {
+		return NULL;
+	}
+
+	BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+	queue<BinaryTreeNode<int>*> pendingNodes;
+	pendingNodes.push(root);
+
+	while(pendingNodes.size() != 0 ){
+		BinaryTreeNode<int>* front = pendingNodes.front();
+		pendingNodes.pop();
+
+		cout << "Enter left child of " << front -> data << " ";
+		int leftChildData;
+		cin >> leftChildData;
+		if(leftChildData != -1){
+			BinaryTreeNode<int>* child = new BinaryTreeNode<int>(leftChildData);
+			front -> left = child;
+			pendingNodes.push(child);
+		}
+
+		cout << "Enter right child of " << front -> data << " ";
+		int rightChildData;
+		cin >> rightChildData;
+		if(rightChildData != -1){
+			BinaryTreeNode<int>* child = new BinaryTreeNode<int>(rightChildData);
+			front -> right = child;
+			pendingNodes.push(child);
+		}		
+	}
+
+	return root; 
+}
 
 void printTree(BinaryTreeNode<int>* root){
 	if(root == NULL){
