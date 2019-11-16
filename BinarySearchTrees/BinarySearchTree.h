c6da8b901e88babf1ca9a75ddc3a1b2885fab3cc
Sachin Gwasikoti <sachingw777@gmail.com>
2019-11-17 04:27:53 +0530

    ADD insert fn

--- BinarySearchTrees/BinarySearchTree.h
+++ BinarySearchTrees/BinarySearchTree.h
@@ -12,18 +12,37 @@ public:
 	~BST(){
 		delete root;
 	}
+private:
+	BinaryTreeNode<int>* insert(int data, BinaryTreeNode<int>* node){
+		if(node == NULL){
+			BinaryTreeNode<int>* newNode = new BinaryTreeNode<int>(data);
+			return newNode;
+		}
+
+		if(node -> data > data){
+			node -> right = insert(node -> right, data);
+		}else{
+			node -> left = insert(node -> left, data);
+		}
+
+		return node;
+}
+public:
+	void insert(int data){
+		this -> root = insert(data, this -> root);		
+	}
 
 private:
 	bool hasData(int data, BinaryTreeNode<int>* node){
-	if(node == NULL){
-		return false;
-	}
+		if(node == NULL){
+			return false;
+		}
 
-	if(node -> data == data){
-		return true;
-	}
+		if(node -> data == data){
+			return true;
+		}
 
-	return hasData(data, node -> left) || hasData(data, node -> right);
+		return hasData(data, node -> left) || hasData(data, node -> right);
 }
 
 public:
