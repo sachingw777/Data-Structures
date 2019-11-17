c0108ddccdef1bae9dacf6cc586cae746f0228a7
Sachin Gwasikoti <sachingw777@gmail.com>
2019-11-17 09:58:10 +0530

    done

---
+++ Hashmaps/intersection.cpp
@@ -0,0 +1,18 @@
+// input1 - first array
+// input2 - second array
+// size1 - size of first array
+// size2 - size of second array
+
+void intersection(int input1[], int input2[], int size1, int size2) {
+    unordered_map<int, int> m;
+    for(int i = 0; i < size1; i++){
+        m[input1[i]]++;
+    }
+
+    for (int i = 0; i < size2; i++)
+    {
+        if(m.count(input2[i]) > 0){
+            cout << input2[i] << endl;
+        }
+    }
+}
