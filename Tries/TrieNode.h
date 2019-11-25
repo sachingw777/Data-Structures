7e755df9cef2cda53cdca450f79083f300e576db
Sachin Gwasikoti <sachingw777@gmail.com>
2019-11-25 12:16:03 +0530

    add array null initialize

--- Tries/TrieNode.h
+++ Tries/TrieNode.h
@@ -6,7 +6,13 @@ public:
 
 	TrieNode(char data){
 		this -> data = data;
-		children = new TrieNode*[26];
+		children = new TrieNode*[26];		
+		
+		for(int i = 0; i < 26; i++){
+			children[i] = NULL;
+		}
+		
 		isTerminal = false
+
 	}
 };
