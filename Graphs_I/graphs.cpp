6b1cc1db01d2ae3f6acb0e5bcd1d85a8f69889da
Sachin Gwasikoti <sachingw777@gmail.com>
2019-11-27 13:45:45 +0530

    add Disconnected Graphs DFS,BFS fn

--- Graphs_I/graphs.cpp
+++ Graphs_I/graphs.cpp
@@ -38,6 +38,35 @@ void printBFS(int** edges, int n, int sv, bool* visited){
 			}
 		}
 	}
+}
+
+void DFS(int** edges, int n){
+	bool* visited = new bool[n];
+	for(int i = 0; i < n; i++){
+		visited[i] = false;
+	}
+
+	for(int i = 0; i < n; i++){
+		if(!visited[i]){
+			printDFS(edges, n, i, visited);
+		}
+	}
+
+	delete [] visited;
+}
+
+void BFS(int** edges, int n){
+	bool* visited = new bool[n];
+	for(int i = 0; i < n; i++){
+		visited[i] = false;
+	}
+
+	for(int i = 0; i < n; i++){
+		if(!visited[i]){
+			printBFS(edges, n, i, visited);
+		}
+	}
+
 	delete [] visited;
 }
 
