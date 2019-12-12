46abb6254201aaddf7465ad01564395f89c27501
Sachin Gwasikoti <sachingw777@gmail.com>
2019-12-12 16:53:46 +0530

    fix.

--- Recursion_II/mergesort.cpp
+++ Recursion_II/mergesort.cpp
@@ -1,50 +1,61 @@
-void merge(int a[], int start, int end){
-    int mid = (start + end)/2;
-    int size = (end - start) + 1;
-    int *output = new int[size];
-    int i = start, j = mid + 1, k = 0;
-
-    while(i <= mid && j <= end){
-
-        if(a[i] <= a[j]){
-            output[k] = a[i];
-            i++;
-        }
-        else {
-            output[k] = a[j];
-            j++;
-        }
-        k++;
-    }
-    
-    while(i <= mid){
-        output[k] = a[i];
-        i++;
-        k++;
+void merge(int input[], int start, int mid, int end) 
+{ 
+    int i, j, k; 
+    int size1 = mid - start + 1; 
+    int size2 =  end - mid; 
+  
+    int L[size1], R[size2]; 
+  
+    for (i = 0; i < size1; i++) {
+        L[i] = input[i + start]; 
     }
 
-    while(j <= end){
-        output[k] = a[j];
-        j++;
-        k++;
-    }
+    for (j = 0; j < size2; j++){
+        R[j] = input[j + mid + 1]; 
+    } 
+  
+    i = 0, j = 0;
+    k =  start;
+    while (i < size1 && j < size2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            input[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            input[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
 
-    for(i = start; i <= end ; i++){
-        a[i] = output[i];
-    }
-    
-    delete [] output;
+    while (i < size1) 
+    { 
+        input[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+    while (j < size2) 
+    { 
+        input[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
 }
 
-void mergeSort(int a[], int start, int end){
+void mergeSort(int input[], int start, int end){
     if(start >= end){
         return;
     }
 
     int mid = (start + end)/2;
+    mergeSort(input, start, mid);
+    mergeSort(input, mid + 1, end);
+    merge(input, start, mid, end);
+}
 
-    mergeSort(a, start, mid);
-    mergeSort(a, mid + 1, end);
-
-    merge(a, start, end);
+void mergeSort(int input[], int size){
+    mergeSort(input, 0, size - 1);
 }
