6bcb8d5cf7326275999e371e01e7fd5f8784535d
Sachin Gwasikoti <sachingw777@gmail.com>
2019-12-19 16:15:06 +0530

    fix

--- Linked_List_I/LinkedListExtra.cpp
+++ Linked_List_I/LinkedListExtra.cpp
@@ -29,11 +29,27 @@ Node* mergeTwoLLs(Node *head1, Node *head2) {
         head2 = head2 -> next;
     }
 
-    while(head1 != NULL || head2 != NULL){
-
+     while(head1 != NULL && head2 != NULL){
+        if(head1 -> data <= head2 -> data){
+            finalTail -> next = head1;
+            head1 = head1 -> next;
+            finalTail = finalTail -> next;
+        }else if(head1 -> data > head2 -> data)
+        {
+            finalTail -> next = head2;
+            head2 = head2 -> next;
+            finalTail = finalTail -> next;
+        }
     }
-    
 
+    if(head1 != NULL){
+        finalTail -> next= head1;
+    }
+    if(head2 != NULL){
+        finalTail -> next= head2;
+    }
+
+    return finalHead;
 }
 
 Node* mergeTwoLLs_rec(Node *head1, Node *head2) {
