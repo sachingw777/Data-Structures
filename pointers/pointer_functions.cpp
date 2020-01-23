c58e81ebcf0331a7bdb24f0c222537b3715d6825
Sachin Gwasikoti <sachingw777@gmail.com>
2020-01-24 00:24:26 +0530

    add checking for revision.

--- pointers/pointer_functions.cpp
+++ pointers/pointer_functions.cpp
@@ -12,9 +12,18 @@ void increment(int * p){
 
 int main(){
     int i = 10;
+    int j = 20;
     int *p = &i;
+
+    const int * p2 = &i;
+    p2 = &j;
+    (*p2)++;
+
     cout << p << endl;
-    increment(p);
+    cout << *p2 << endl;
+    // increment(p);
     // cout << p << endl; 
-    cout << *(p);
+    // cout << *(p);
+
+
 }
