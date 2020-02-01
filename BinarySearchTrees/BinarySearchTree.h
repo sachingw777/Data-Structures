52d9bd9401e4ef74e254bc1969c123a6f045ebf8
Sachin Gwasikoti <sachingw777@gmail.com>
2020-02-01 06:27:37 +0530

    add deleteData, insert fix.

--- BinarySearchTrees/BinarySearchTree.h
+++ BinarySearchTrees/BinarySearchTree.h
@@ -12,23 +12,51 @@ public:
 	~BST(){
 		delete root;
 	}
+
 private:
-	BinaryTreeNode delete(int data, BinaryTreeNode<int>* node){
-		if(root == NULL){
+	BinaryTreeNode<int>* deleteData(int data, BinaryTreeNode<int>* node){
+		if(node == NULL){
 			return NULL;
 		}
 
-		
+		if(data > node -> data){
+			node -> right = deleteData(data, node -> right);
+			return node;
+		}else if( data < node -> data){
+			node -> left = deleteData(data, node -> left);
+			return node;
+		}else{
+			if(node -> left == NULL && node -> right == NULL){	//NO CHILD
+				delete node;
+				return NULL;
+			}else if(node -> right != NULL && node -> left == NULL){
+				BinaryTreeNode<int>* temp = node -> right;
+				node -> right = NULL;
+				delete node;
+				return temp;
+			}else if(node -> left != NULL && node -> right == NULL){
+				BinaryTreeNode<int>* temp = node -> left;
+				node -> left = NULL;
+				delete node;
+				return temp;
+			}else{
+				//find min of right side
+				BinaryTreeNode<int>* temp = node -> right;
+				while(temp -> left != NULL){
+					temp = temp -> left;
+				}
+				node -> data = temp -> data;
+				node -> right = deleteData(temp -> data, node -> right);
+				return node;
+			}
+		}
 	}
 
 public:
 	void deleteData(int data){
-
+		this -> root = deleteData(data, this -> root);
 	}
 
-	void printTree(){
-		
-	}
 private:
 	BinaryTreeNode<int>* insert(int data, BinaryTreeNode<int>* node){
 		if(node == NULL){
@@ -37,13 +65,14 @@ private:
 		}
 
 		if(node -> data > data){
-			node -> right = insert(node -> right, data);
+			node -> right = insert(data,node -> right);
 		}else{
-			node -> left = insert(node -> left, data);
+			node -> left = insert(data, node -> left);
 		}
 
 		return node;
 }
+
 public:
 	void insert(int data){
 		this -> root = insert(data, this -> root);		
