c06f6816764480095084c803d5ead8d6fe0f756e
Sachin Gwasikoti <sachingw777@gmail.com>
2020-02-01 06:27:59 +0530

    add printTree

--- BinarySearchTrees/BinarySearchTree.h
+++ BinarySearchTrees/BinarySearchTree.h
@@ -1,7 +1,7 @@
-#include "BinaryTreeNode.h"
+#include <iostream>
 using namespace std;
 
-class BST(){
+class BST{
 	BinaryTreeNode<int>* root;
 
 public:
@@ -95,5 +95,21 @@ public:
 	bool hasData(int data){
 		return hasData(data, root);
 	}
+    
+    void printTree(BinaryTreeNode<int>* root){
+        if(root == NULL){
+            return;
+        }
 
+        cout << root -> data << ":";
+        if(root -> left != NULL){
+            cout << "L"<< root -> left -> data << ",";
+        }
+        if(root -> right != NULL){
+            cout << "R"<< root -> right -> data;
+        }
+        cout << endl;
+        printTree(root -> left);
+        printTree(root -> right); 
+    }
 };
