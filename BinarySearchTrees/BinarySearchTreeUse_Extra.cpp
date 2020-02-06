db4e5d45ecef91829ee5228b3df57ce2ae9fe1b9
Sachin Gwasikoti <sachingw777@gmail.com>
2020-02-06 11:54:16 +0530

    add lca binary tree.

--- BinarySearchTrees/BinarySearchTreeUse_Extra.cpp
+++ BinarySearchTrees/BinarySearchTreeUse_Extra.cpp
@@ -17,9 +17,37 @@ void insertDuplicateNode(BinaryTreeNode<int> *root) {
     newNode -> left = temp;
 }
 
+BinaryTreeNode<int>* lca(BinaryTreeNode<int>* root , int val1, int val2){
+	if(root == NULL){
+		return root;
+	}
+
+	if(root -> data == val1 || root -> data == val2){
+		return root;
+	}
+
+	BinaryTreeNode<int>* leftAns = lca(root -> left, val1, val2);
+	BinaryTreeNode<int>* rightAns = lca(root -> right, val1, val2);
+
+	if(leftAns != NULL && rightAns != NULL){
+		return root;
+	}
+
+	if(leftAns == NULL && rightAns == NULL){
+		return NULL;
+	}
+
+    return leftAns != NULL ? leftAns : rightAns;
+}
+
+int lcaBinaryTree(BinaryTreeNode<int>* root , int val1, int val2){
+	BinaryTreeNode<int>* ans = lca(root, val1, val2);
+	return ans -> data;
+}
+
 int main() {
-    BinaryTreeNode<int>* root = takeInput();
-    insertDuplicateNode(root);
-    printLevelATNewLine(root);
-    delete root;
+    // BinaryTreeNode<int>* root = takeInput();
+    // insertDuplicateNode(root);
+    // printLevelATNewLine(root);
+    // delete root;
 }
