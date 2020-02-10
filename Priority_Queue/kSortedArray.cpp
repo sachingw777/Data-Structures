688a0febe613455d642a8e4e06db38c0eba2b447
Sachin Gwasikoti <sachingw777@gmail.com>
2020-02-10 19:18:19 +0530

    add kSortedArray.

---
+++ Priority_Queue/kSortedArray.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void kSortedArray(int input[], int n, int k){
+	priority_queue<int> pq;
+	for(int i = 0; i < k; i++){		//fill pq
+		pq.push(input[i]);
+	}
+
+	int j = 0;
+	for(int i = k; i < n; i++){
+		input[j] = pq.top();
+		pq.pop();
+		pq.push(input[k]);
+		j++;
+	}
+
+	while(!pq.empty()){
+		input[j] = pq.top();
+		pq.pop();
+	}
+}
+int main() {
+	int input[] = {10, 12, 6, 7, 9};
+	int k = 3;
+	kSortedArray(input, 5, k);
+	for(int i = 0; i < 5; i++) {
+		cout << input[i] << " ";
+	}
+}
