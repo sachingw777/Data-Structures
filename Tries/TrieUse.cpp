440b3a7438dd01bc92c7b5c0f8ed966d67b6388e
Sachin Gwasikoti <sachingw777@gmail.com>
2020-02-11 19:56:08 +0530

    add patternMatching main code.

--- Tries/TrieUse.cpp
+++ Tries/TrieUse.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 #include "Trie.h"
+#include <vector>
+
 
 int main(){
 	Trie t;
@@ -12,4 +14,24 @@ int main(){
 
 	t.removeWord("are");
 	cout << t.search("are") << endl;
+
+/* 
+//pattern matching
+	Trie t;
+    int N;
+    cin >> N;
+    string pattern;
+    vector<string> vect;
+    for (int i=0; i < N; i++) {
+        string word;
+        cin >> word;
+        vect.push_back(word);
+    }
+    cin >> pattern;
+    if (t.patternMatching(vect, pattern)) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+    */
 }
