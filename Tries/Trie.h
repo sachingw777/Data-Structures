b88ea79a6d5b18b14baae236cd4b9ea0b53ea289
Sachin Gwasikoti <sachingw777@gmail.com>
2020-02-11 17:57:56 +0530

    add removeWord fn.

--- Tries/Trie.h
+++ Tries/Trie.h
@@ -1,3 +1,6 @@
+#include "TrieNode.h"
+#include <string>
+
 class Trie{
 	TrieNode* root;
 
@@ -6,6 +9,7 @@ public:
 		root = new TrieNode('\0');
 	}
 
+private:
 	void insertWord(TrieNode *root, string word){
 		if(word.size() == 0){
 			root -> isTerminal = true;
@@ -24,10 +28,12 @@ public:
 		insertWord(child, word.substr(1));
 	}
 
+public:
 	void insertWord(string word){
 		insertWord(root, word);
 	}
 
+private:
     bool search(TrieNode *root, string word) {
 		if(word.size() == 0){
 			return root -> isTerminal;
@@ -45,7 +51,43 @@ public:
 		return search(child, word.substr(1));
     }
 
+public:
     bool search(string word){
     	return search(root, word);
     }
+
+private:
+	void removeWord(TrieNode* root, string word){
+		if(word.size() == 0){
+			root -> isTerminal = false;
+			return;
+		}
+
+		int index = word[0] - 'a';
+		TrieNode* child;
+
+		if(root -> children[index] != NULL){
+			child = root -> children[index];
+		}else{
+			return;	//word not found
+		}
+
+		removeWord(child, word.substr(1));
+
+		//delete node if it is useless. ('a' in case of 'and') Assume rest is all deleted, hyptohesis.  
+		if(child -> isTerminal == false){
+			for(int i = 0; i < 26; i++){
+				if(child -> children[i] != NULL){
+					return;
+				}
+			}
+			delete child;	//deletes node. 'a in case of 'and'
+			root -> children[index] = NULL; 	//root is '\0' so we travel to root -> children[index of 'a' in case of 'and'] and mark it as NULL 		}
+		}
+	}
+
+public:
+	void removeWord(string word){
+		removeWord(root, word);
+	}
 };
