3b9d367494256135b18a76af8f32b95945639164
Sachin Gwasikoti <sachingw777@gmail.com>
2020-02-15 18:55:41 +0530

    add getPathBFS().

--- Graphs_I/getPath.cpp
+++ Graphs_I/getPath.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <unordered_map>
 using namespace std;
 
-vector<int>* getPath(int** edges, int n, int sv, int ev, bool* visited){
+vector<int>* getPathDFS(int** edges, int n, int sv, int ev, bool* visited){
     if(sv == ev){
         vector<int>* ans = new vector<int>;
         ans->push_back(ev);
@@ -13,7 +15,7 @@ vector<int>* getPath(int** edges, int n, int sv, int ev, bool* visited){
     
     for(int i = 0; i < n; i++){
         if(edges[sv][i] == 1 && !visited[i]){
-            vector<int>* small = getPath(edges, n, i, ev, visited);
+            vector<int>* small = getPathDFS(edges, n, i, ev, visited);
             if(small != NULL){
                 small->push_back(sv);
                 return small;
@@ -23,17 +25,63 @@ vector<int>* getPath(int** edges, int n, int sv, int ev, bool* visited){
     return NULL;
 }
 
-vector<int>* getPath(int** edges, int n, int sv, int ev){
+vector<int>* getPathDFS(int** edges, int n, int sv, int ev){
     bool* visited = new bool[n];
     for(int i = 0; i < n; i++){
         visited[i] = false;
     }
    
-    vector<int>* ans = getPath(edges, n, sv, ev, visited);
+    vector<int>* ans = getPathDFS(edges, n, sv, ev, visited);
     delete [] visited;
     return ans;
 }
 
+
+vector<int>* getPathBFS(int** edges, int n, int sv, int ev){
+    vector<int>* output = new vector<int>;
+    unordered_map<int, int> map;
+    queue<int> q;
+    q.push(sv);
+    bool* visited = new bool[n];
+
+    for(int i = 0; i < n; i++){
+        visited[i] = false;
+    }
+
+    visited[sv] = true;
+    bool flag = false;
+
+    while(!q.empty()){
+        int front = q.front();
+        q.pop();
+
+        for(int i = 0; i < n; i++){
+            if(edges[front][i] == 1 && !visited[i]){
+                map[i] = front;
+                q.push(i);
+                visited[i] = true;
+
+                if(i == ev){
+                    flag = true;
+                    break;
+                }
+            }
+        }
+    }
+    if(!flag){
+        return NULL;
+    }else{
+        int current = ev;
+        output->push_back(ev);
+        while(current != sv){
+            current = map[current];
+            output->push_back(current);
+        }
+    }
+
+    delete [] visited;
+    return output;
+}
 int main(){
     
     int n;
@@ -59,13 +107,25 @@ int main(){
     int tempX, tempY;
     cin >> tempX >> tempY;
     
-    vector<int>* ans = getPath(edges, n, tempX, tempY);
-    if(ans != NULL){
-        for(int i = 0; i < ans->size(); i++){
-            cout << ans->at(i) << " ";      
+    vector<int>* ansDFS = getPathDFS(edges, n, tempX, tempY);
+    vector<int>* ansBFS = getPathBFS(edges, n, tempX, tempY);
+
+    cout << "DFS Path:- " << endl;
+    if(ansDFS != NULL){
+        for(int i = 0; i < ansDFS->size(); i++){
+            cout << ansDFS->at(i) << " ";      
         }   
     }    
-    delete ans;
+    delete ansDFS;
+
+    cout << endl <<  "BFS Path:- " << endl;
+    if(ansBFS != NULL){
+        for(int i = 0; i < ansBFS->size(); i++){
+            cout << ansBFS->at(i) << " ";      
+        }   
+    }    
+    delete ansBFS;
+
     
     for(int i = 0; i < n; i++){
         delete [] edges[i];
